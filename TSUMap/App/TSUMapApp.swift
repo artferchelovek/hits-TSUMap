8a69a310054795957f03cdfb11f7c8fcdd40a6b6
Artem Shutov <artfergames@gmail.com>
2026-03-17 23:39:49 +0700

    init REPO

---
+++ TSUMap/App/TSUMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSUMapApp.swift
+//  TSUMap
+//
+//  Created by Artem on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TSUMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
