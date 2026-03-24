c2a5ec44390f93d5b51c1f7cb83afecb169f22e8
bunnyyrr <kate_kondrashova07@mail.ru>
2026-03-24 18:17:13 +0700

    дерево решений

--- TSUMap/Algorithms/decisionTree/tree.swift
+++ TSUMap/Algorithms/decisionTree/tree.swift
@@ -7,51 +7,12 @@
 
 import Foundation
 
-// MARK: временно переделал под Node -> TreeNode
-class TreeNode {
-    var attributeName: String?
-    var children: [String: TreeNode] = [:]
-    var result: String?
-    var defaultResult: String?
-
-    init(attributeName: String, defaultResult: String? = nil) {
-        self.attributeName = attributeName
-        self.defaultResult = defaultResult
-    }
-    init (result: String) {
-        self.result = result
-    }
-}
-
-func predict(tree: TreeNode, situation: Attribute) -> (result: String, path: [TreeNode]) {
-    var currentNode = tree
-    var path: [TreeNode] = []
-
-    while currentNode.result == nil {
-        path.append(currentNode)
-
-        guard let attribute = currentNode.attributeName else { break }
-        let value = getValue(from: situation, for: attribute)
-
-        if let nextNode = currentNode.children[value] {
-            currentNode = nextNode
-        } else {
-            let fallback = currentNode.defaultResult ?? "Неизвестно"
-            return (fallback, path)
-        }
-    }
-    path.append(currentNode
-    )
-    let finalResult = currentNode.result ?? "Ошибка"
-    return (finalResult, path)
-}
-
-func Entropy(_ data: [Attribute]) -> Double {
+private func Entropy(_ data: [treeAttribute]) -> Double {
     var counts: [String: Double] = [:]
     for item in data {
         counts[ item.recommended_place, default: 0.0] += 1.0
     }
-
+    
     let totalCount = Double(data.count)
     var entropy = 0.0
     for count in counts.values {
@@ -61,12 +22,12 @@ func Entropy(_ data: [Attribute]) -> Double {
     return entropy
 }
 
-func InformationGain(from data: [Attribute], for columnName: String) -> Double {
+private func InformationGain(from data: [treeAttribute], for columnName: String) -> Double {
     let totalEntropy = Entropy(data)
-
-    var groups: [String: [Attribute]] = [:]
+    
+    var groups: [String: [treeAttribute]] = [:]
     for item in data {
-        let value = getValue(from: item, for: columnName)
+        let value = getAttribute(from: item, for: columnName)
         groups[value, default: []].append(item)
     }
     var entropy = 0.0
@@ -76,23 +37,38 @@ func InformationGain(from data: [Attribute], for columnName: String) -> Double {
     return totalEntropy - entropy
 }
 
-func buildTree(data: [Attribute], availableAttributes: [String]) -> TreeNode {
+class TreeNode {
+    var attributeName : String?
+    var children : [String: TreeNode] = [:]
+    var result : String?
+    var defaultResult: String?
+    
+    init(attributeName: String, defaultResult: String? = nil) {
+        self.attributeName = attributeName
+        self.defaultResult = defaultResult
+    }
+    init (result: String) {
+        self.result = result
+    }
+}
+
+func buildTree(data: [treeAttribute], availableAttributes: [String]) -> TreeNode {
     let element = data[0].recommended_place
-    let allSame = data.allSatisfy {$0.recommended_place == element}
+    let allSame = data.allSatisfy{$0.recommended_place == element}
     if allSame {
         return TreeNode(result: element)
     }
-
+    
     var counts: [String: Int] = [:]
     for item in data {
         counts[item.recommended_place, default: 0] += 1
     }
     let mostCommon = counts.max(by: { $0.value < $1.value })!.key
-
+       
     if availableAttributes.isEmpty {
         return TreeNode(result: mostCommon)
     }
-
+    
     var bestAttribute = ""
     var bestGain = -1.0
     for attribute in availableAttributes {
@@ -102,25 +78,47 @@ func buildTree(data: [Attribute], availableAttributes: [String]) -> TreeNode {
             bestAttribute = attribute
         }
     }
-
+    
     let node = TreeNode(attributeName: bestAttribute, defaultResult: mostCommon)
-    let updatedAttribute = availableAttributes.filter {$0 != bestAttribute}
-
-    var groups: [String: [Attribute]] = [:]
+    let updatedAttribute = availableAttributes.filter{$0 != bestAttribute}
+    
+    var groups: [String: [treeAttribute]] = [:]
     for item in data {
-        let value = getValue(from: item, for: bestAttribute)
+        let value = getAttribute(from: item, for: bestAttribute)
         if groups[value] == nil {
             groups[value] = [item]
         } else {
             groups[value, default: []].append(item)
         }
     }
-
+    
     for (branchValue, BranchData) in groups {
         let childNode = buildTree(data: BranchData, availableAttributes: updatedAttribute)
         node.children[branchValue] = childNode
     }
-    var temp = node
+    return node
+}
 
-    return temp
+func predict(tree: TreeNode, situation: treeAttribute) -> (result: String, path: [TreeNode]) {
+    var currentNode = tree
+    var path: [TreeNode] = []
+    
+    while currentNode.result == nil {
+        path.append(currentNode)
+        
+        guard let attribute = currentNode.attributeName else { break }
+        let value = getAttribute(from: situation, for: attribute)
+        
+        if let nextNode = currentNode.children[value] {
+            currentNode = nextNode
+        }
+        else {
+            let fallback = currentNode.defaultResult ?? "Неизвестно"
+            return (fallback, path)
+        }
+    }
+    path.append(currentNode
+    )
+    let finalResult = currentNode.result ?? "Ошибка"
+    return (finalResult, path)
 }
--- TSUMap/Algorithms/decisionTree/treeAttribute.swift
+++ TSUMap/Algorithms/decisionTree/treeAttribute.swift
@@ -1,4 +1,4 @@
-struct Attribute {
+struct treeAttribute {
     var location: String
     var budget: String
     var time_available: String
