382902794198273f594965830c45c462b12baab1
bunnyyrr <kate_kondrashova07@mail.ru>
2026-04-02 21:08:26 +0700

    fix: decisionTree

--- TSUMap/Algorithms/decisionTree/tree.swift
+++ TSUMap/Algorithms/decisionTree/tree.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-// MARK: временно переделал под Node -> TreeNode
 class TreeNode {
     var attributeName: String?
     var children: [String: TreeNode] = [:]
@@ -23,30 +22,7 @@ class TreeNode {
     }
 }
 
-func predictTree(tree: TreeNode, situation: TreeAttribute) -> (result: String, path: [TreeNode]) {
-    var currentNode = tree
-    var path: [TreeNode] = []
-    
-    while currentNode.result == nil {
-        path.append(currentNode)
-        
-        guard let attribute = currentNode.attributeName else { break }
-        let value = getTreeAttribute(from: situation, for: attribute)
-        
-        if let nextNode = currentNode.children[value] {
-            currentNode = nextNode
-        } else {
-            let fallback = currentNode.defaultResult ?? "Неизвестно"
-            return (fallback, path)
-        }
-    }
-    path.append(currentNode
-    )
-    let finalResult = currentNode.result ?? "Ошибка"
-    return (finalResult, path)
-}
-
-func Entropy(_ data: [TreeAttribute]) -> Double {
+private func Entropy(_ data: [TreeAttribute]) -> Double {
     var counts: [String: Double] = [:]
     for item in data {
         counts[ item.recommended_place, default: 0.0] += 1.0
@@ -61,7 +37,7 @@ func Entropy(_ data: [TreeAttribute]) -> Double {
     return entropy
 }
 
-func InformationGain(from data: [TreeAttribute], for columnName: String) -> Double {
+private func InformationGain(from data: [TreeAttribute], for columnName: String) -> Double {
     let totalEntropy = Entropy(data)
     
     var groups: [String: [TreeAttribute]] = [:]
@@ -120,6 +96,28 @@ func buildTree(data: [TreeAttribute], availableAttributes: [String]) -> TreeNode
         let childNode = buildTree(data: BranchData, availableAttributes: updatedAttribute)
         node.children[branchValue] = childNode
     }
-    
     return node
+}
+
+func predictTree(tree: TreeNode, situation: TreeAttribute) -> (result: String, path: [TreeNode]) {
+    var currentNode = tree
+    var path: [TreeNode] = []
+    
+    while currentNode.result == nil {
+        path.append(currentNode)
+        
+        guard let attribute = currentNode.attributeName else { break }
+        let value = getTreeAttribute(from: situation, for: attribute)
+        
+        if let nextNode = currentNode.children[value] {
+            currentNode = nextNode
+        } else {
+            let fallback = currentNode.defaultResult ?? "Неизвестно"
+            return (fallback, path)
+        }
+    }
+    path.append(currentNode
+    )
+    let finalResult = currentNode.result ?? "Ошибка"
+    return (finalResult, path)
 }
